3cf81dfd9a1be681b1c46e861a525a7b35e154a1
Vitalii900 <melnikvitaliy30@gmail.com>
2022-11-27 17:45:10 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,23 @@
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', function (e) {
+player.on('timeupdate', getCurrentTime);
+
+function getCurrentTime(e) {
   // console.log(e.seconds);
   localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
+}
 
+function startAfterReload() {
   const currentTime = localStorage.getItem('videoplayer-current-time');
   const parcedTime = JSON.parse(currentTime);
 
-  if (parcedTime) {
-    // console.log(parcedTime);
+  if (currentTime) {
+    player.setCurrentTime(parcedTime);
   }
-});
+}
+
+startAfterReload();
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
