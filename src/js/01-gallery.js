6a0ddf05b49c9e19df87225c94a4fd1c3eede9c0
Vitalii900 <melnikvitaliy30@gmail.com>
2022-11-27 09:47:29 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+function createImages(items) {
+  return items
+    .map(
+      item =>
+        `<a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>`
+    )
+    .join('');
+}
+
+const addImagesToHtml = createImages(galleryItems);
+// console.log(addImagesToHtml);
+
+galleryRef.innerHTML = addImagesToHtml;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+});
