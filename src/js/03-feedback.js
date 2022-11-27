59cec50a1b15ba7fc97bb21ba9cf250110711a0e
Vitalii900 <melnikvitaliy30@gmail.com>
2022-11-27 19:41:25 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+const formRef = document.querySelector('form');
 
+const inputRef = document.querySelector('input');
+
+const textareaRef = document.querySelector('textarea');
+
+formRef.addEventListener('input', onFormInput);
+
+formRef.addEventListener('submit', onFormSubmit);
+
+check();
+
+function onFormInput(evt) {
+    const email = evt.currentTarget.elements.email.value;
+    const message = evt.currentTarget.elements.message.value;
+
+    const formValues = JSON.stringify({ email, message });
+
+    localStorage.setItem('feedback-form-state', formValues);
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    const elements = evt.currentTarget.elements;
+
+    console.log(`email: ${elements.email.value}`);
+
+    console.log(`message: ${elements.message.value}`);
+    evt.currentTarget.reset();
+    localStorage.clear();
+}
+
+function check() {
+    const valuesForCheck = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+    );
+
+//   console.log(valuesForCheck.email.length);
+
+    if (valuesForCheck.email.length !== 0 || valuesForCheck.message.length !== 0) {
+        inputRef.value = valuesForCheck.email;
+        textareaRef.value = valuesForCheck.message;
+    }
+}
